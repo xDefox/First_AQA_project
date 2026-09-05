d4cadf58ce727ac9885076d532895be51cc00f38
Куга <celerval5@gmail.com>
2026-09-05 15:15:52 +0300

    test_main_page optmization

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
 
 def go_to_login_page(browser):
@@ -7,5 +8,6 @@ def go_to_login_page(browser):
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page(browser) 
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
