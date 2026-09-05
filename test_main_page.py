4dee015c411e76e1ffd9ab7eed10ee45f25a0b59
Куга <celerval5@gmail.com>
2026-09-05 15:24:43 +0300

    exception update

--- test_main_page.py
+++ test_main_page.py
@@ -6,8 +6,9 @@ def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
 
-def test_guest_can_go_to_login_page(browser):
+
+def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
